4a60c3f7b4a29e264348bfcebb2a10c74306ca42
raihanraju007 <raihanraju007@gmail.com>
2026-06-01 18:56:43 +0600

    docker file setup complate

---
+++ src/main/java/com/raju/enterprise/springboot_industry_practice/SpringbootIndustryPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.raju.enterprise.springboot_industry_practice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootIndustryPracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootIndustryPracticeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/raju/enterprise/springboot_industry_practice/SpringbootIndustryPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.raju.enterprise.springboot_industry_practice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootIndustryPracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
